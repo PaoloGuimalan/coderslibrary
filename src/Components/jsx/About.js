42ea235731ce747a59f0185d4e1edf831d6efc14
PaoloGuimalan <pauloportes.guimalan187@gmail.com>
2021-09-09 13:43:06 -0800

    first commit

--- src/Components/jsx/About.js
+++ src/Components/jsx/About.js
@@ -4,7 +4,13 @@ import '../css/About.css'
 function About() {
     return (
         <div id='about_div'>
-            Hello from About
+            <div>
+                <p id='label_about'>About Coder's Library</p>
+            </div>
+            <div id='div_sec_ab'>
+                <p className='ab_parags'>Coder's Library were created mainly for sharing our team's gathered books. During the pandemic we as learners also struggle to find enough resources for us to learn more about Programming, Coding and other IT based books and topics. Our team began collecting free pdf books so that we can easily share them as a whole.</p>
+                <p className='ab_parags'>Our project started in September 2021 and hopefully will become progressive. Coder's Library is here to help learners to attain resources/books that are needed for them to attain their best capacity in learning, specifically in Coding and Programming.</p>
+            </div>
         </div>
     )
 }
