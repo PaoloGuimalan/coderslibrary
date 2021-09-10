378ba75c02992bcc1d849bc5a9e2d1e2dd3cdc5a
PaoloGuimalan <pauloportes.guimalan187@gmail.com>
2021-09-09 23:10:46 -0800

    first commit

--- src/Components/jsx/Books.js
+++ src/Components/jsx/Books.js
@@ -4,10 +4,25 @@ import Book from '../imgs/book.png';
 import {Link} from 'react-router-dom';
 import {motion} from 'framer-motion';
 import {useState} from 'react';
+import datamain from '../../Data/data_values';
 
 function Books() {
 
     const [sidep, setsidep] = useState(false);
+    const [setter, setsetter] = useState(true);
+
+    const unset = (nm) => {
+        //alert(nm);
+        setsetter(!setter);
+        if(setter == true)
+        {
+            document.getElementById(nm).style.display = "none";
+        }
+        else
+        {
+            document.getElementById(nm).style.display = "unset";
+        }
+    }
 
     return (
         <div id='div_books'>
@@ -15,7 +30,12 @@ function Books() {
                 <motion.div 
                     animate={{
                         width: window.innerWidth < 768 ? sidep == false ? "50px" : "100%" : sidep == false ? "50px" : "20%",
-                        height: window.innerWidth < 768 ? sidep == false ? "50px" : "100%" : sidep == false ? "100%" : "100%"
+                        height: window.innerWidth < 768 ? sidep == false ? "50px" : "100%" : sidep == false ? "100%" : "100%",
+                        overflowY: sidep == false ? "hidden" : "scroll",
+                        overflowX: sidep == false ? "hidden" : "hidden",
+                        transition:{
+                            delay: sidep == false ? 0.5 : 0
+                        }
                     }}
                 id='sidepanel'>
                     <motion.button 
@@ -24,7 +44,43 @@ function Books() {
                             borderRadius: sidep == false? "0px" : "50px"
                         }}
                     id='btn_panel' onClick={() => {setsidep(!sidep);}}>&gt;</motion.button>
+
+                    <motion.table id='tbl_cat'
+                        animate={{
+                            opacity: sidep == false ? 0 : 1,
+                            transition:{
+                                delay: sidep == false ? 0 : 0.5
+                            }
+                        }}
+                    >
+                        <tbody>
+                            <tr>
+                                <td>
+                                    <p id='label_cat'>Categories</p>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>
+                                    {datamain.categories.map((val) => {
+                                        //console.log(val);
+                                        return <p>{<p><span className='val_handler' onClick={() => {unset(val)}}>{val}</span>
+                                        <table className='tbls_result' id={val}>{
+                                            datamain.books.map((val2) => {
+                                                if(val == val2.category)
+                                                {
+                                                    //console.log(val2.name);
+                                                    return <p className='val2_holder'>{val2.name}</p>
+                                                }
+                                            })}</table></p>
+                                        }</p>
+                                    })}
+                                </td>
+                            </tr>
+                        </tbody>
+                    </motion.table>
                 </motion.div>
+
+                
                 <table className='tbl_prev'>
                     <tbody>
                         <tr>
---
+++ src/Data/data_values.js
@@ -0,0 +1,50 @@
+const data_books = {
+    categories:[
+        "Algorithms",
+        "Android",
+        "Angular",
+        "Arduino",
+        "C",
+        "C#",
+        "C++",
+        "CSS",
+        "Databases",
+        "HTML",
+        "IOS",
+        "Java",
+        "JavaScript",
+        "JQuery",
+        "Linux",
+        "Matlab",
+        "Microsoft",
+        "PHP",
+        "Python",
+        "Raspberry Pi",
+        "Ruby",
+        "Swift"
+    ],
+    books:[
+        {
+            id: "1",
+            category: "Algorithms",
+            name: "Algorithm Notes for Pros"
+        },
+        {
+            id: "2",
+            category: "Algorithms",
+            name: "Algorithms for Dummies"
+        },
+        {
+            id: "3",
+            category: "Android",
+            name: "Android Development"
+        },
+        {
+            id: "12",
+            category: "Ruby",
+            name: "Ruby Marie"
+        }
+    ]
+}
+
+export default data_books;
