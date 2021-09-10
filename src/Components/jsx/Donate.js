fc0cb35955a62e57526b06e9758aed15c05a9b06
PaoloGuimalan <pauloportes.guimalan187@gmail.com>
2021-09-09 17:59:56 -0800

    first commit

--- src/Components/jsx/Donate.js
+++ src/Components/jsx/Donate.js
@@ -1,10 +1,25 @@
 import React from 'react'
+import gcash from '../imgs/gcash.png';
+import paymaya from '../imgs/paymaya.png';
 import '../css/Donate.css'
 
 function Donate() {
     return (
         <div id='donate_div'>
-            Hello from Donate
+            <div>
+                <p id='label_donate'>You can donate at the following.</p>
+            </div>
+            <div id='div_imgs'>
+                <img className='img_donate' src={gcash}></img>
+                <center><p><b>Account Number:</b> 09275508232</p></center>
+            </div>
+            <div id='div_imgs'>
+                <img className='img_donate' src={paymaya}></img>
+                <center><p><b>Account Number:</b> 09275508232</p></center>
+            </div>
+            <div>
+                <p id='label_donate_foot'>Thank You for your support! :></p>
+            </div>
         </div>
     )
 }
