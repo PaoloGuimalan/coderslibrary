825f97ff277783a5dd8df268bfdb086c2f31fa94
PaoloGuimalan <pauloportes.guimalan187@gmail.com>
2022-07-13 01:24:58 -0900

    Download Android App Page has been Added

---
+++ src/Components/data/android.js
@@ -0,0 +1,47 @@
+import Splash from '../res/android/splash.jpg'
+import Home from '../res/android/home.jpg'
+import Search from '../res/android/Search.jpg'
+import Books from '../res/android/Books.jpg'
+import Marks from '../res/android/Marks.jpg'
+import Settings from '../res/android/Settings.jpg'
+import ViewBook from '../res/android/ViewBook.jpg'
+import Comments from '../res/android/Comments.jpg'
+import Bookmarks from '../res/android/Bookmarks.jpg'
+import ViewBookOffline from '../res/android/ViewBookOffline.jpg'
+import Notifications from '../res/android/Notifications.jpg'
+
+export const android = [
+    {
+        img: Splash
+    },
+    {
+        img: Home
+    },
+    {
+        img: Search
+    },
+    {
+        img: Books
+    },
+    {
+        img: Marks
+    },
+    {
+        img: Settings
+    },
+    {
+        img: ViewBook
+    },
+    {
+        img: Comments
+    },
+    {
+        img: Bookmarks
+    },
+    {
+        img: ViewBookOffline
+    },
+    {
+        img: Notifications
+    }
+]
--- src/Components/jsx/PlatformAndroid.js
+++ src/Components/jsx/PlatformAndroid.js
@@ -1,8 +1,95 @@
 import React from 'react'
+import '../css/Platform.css'
+// import { Carousel } from 'react-responsive-carousel'
+// import 'react-responsive-carousel/lib/styles/carousel.min.css'
+import Carousel from 'react-multi-carousel'
+import "react-multi-carousel/lib/styles.css";
+import { android } from '../data/android'
+import Splash from '../res/android/splash.jpg'
+import Home from '../res/android/home.jpg'
 
 function PlatformAndroid() {
+
+  const responsive = {
+    superLargeDesktop: {
+      // the naming can be any, depends on you.
+      breakpoint: { max: 4000, min: 3000 },
+      items: 10
+    },
+    desktop: {
+      breakpoint: { max: 3000, min: 1024 },
+      items: 4.7
+    },
+    tablet: {
+      breakpoint: { max: 1024, min: 464 },
+      items: 2
+    },
+    mobile: {
+      breakpoint: { max: 464, min: 0 },
+      items: 1.7
+    }
+  };
+
+  const downloadAPK = (version) => {
+    if(version == 1){
+      window.open('https://github.com/PaoloGuimalan/coderslibrary_app/blob/main/productionBuild/CodersLibrary.apk?raw=true','_blank')
+    }
+  }
+
   return (
-    <div>PlatformAndroid</div>
+    <div id='div_platform'>
+      <div id='div_platform_label'>
+        <p id='tag_platform_label'>Coder's Library App</p>
+        <p id='tag_label_device'>Android</p>
+      </div>
+      <div id='carousel_holder'>
+          <Carousel responsive={responsive} swipeable={true} draggable={true}>
+            {android.map((items, i) => {
+              return(
+                <img key={i} src={items.img} style={{width: 200, height: "100%", border: "solid 1px grey"}} />
+              )
+            })}
+          </Carousel>
+      </div>
+      <div id='div_statement'>
+        <p id='tag_statement'>
+          This is one of the new services Coder's Library offers. Having multiple demands of materials we had. We have 
+          decided to release an App Version of our System which now, provides Account Management, Offline Mode and many 
+          more. This Version of it is an Android Version where it can be installed in any device you desire that holds 
+          and Android OS from versions Android 5.0 (Lollipop) up to the latest.
+        </p>
+      </div>
+      <div id='div_statement'>
+        <p id='label_features'>Features</p>
+        <p id='features_statement'>
+          -Account Management
+        </p>
+        <p id='features_statement'>
+          -Recents History
+        </p>
+        <p id='features_statement'>
+          -Read inside the App
+        </p>
+        <p id='features_statement'>
+          -Bookmark Pages
+        </p>
+        <p id='features_statement'>
+          -Download Books / Offline Mode
+        </p>
+        <p id='features_statement'>
+          -Comment / Tag someone on a Book
+        </p>
+        <p id='features_statement'>
+          -Like/Heart/Save a Book
+        </p>
+      </div>
+      <div id='div_downloads'>
+        <p id='label_features'>Downloads</p>
+        <div id='div_selection_downloads'>
+          <button onClick={() => { downloadAPK(1) }} className='button_downloads_version'>APK Version 1.0</button>
+        </div>
+      </div>
+    </div>
   )
 }
 
